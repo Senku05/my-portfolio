0e91c0c1aad739b1b27582a65f5f944565b02d7a
Senku05 <lelemol05@gmail.com>
2023-05-30 20:52:50 +0200

    Edit files

--- maintenance.js
+++
@@ -1,7 +0,0 @@
-// Verifica se il sito è in manutenzione
-var maintenanceMode = false;
-
-// Se il sito è in manutenzione, reindirizza tutte le richieste alla pagina di manutenzione
-if (maintenanceMode && location.pathname !== '/maintenance.html') {
-  window.location.href = '/maintenance.html';
-}
